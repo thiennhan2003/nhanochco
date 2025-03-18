401b55dd7657af3801496a40d2ab7f90d6e203b0
AnthanBiPhap <paimon0000000000@gmail.com>
2025-03-19 00:26:14 +0700

    fontend

--- Fontend/src/App.tsx
+++ Fontend/src/App.tsx
@@ -6,7 +6,7 @@ import Restaurants from "./components/Restaurants";
 import Posts from "./components/Posts";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import RestaurantsPage from './pages/RestaurantsPage'; // Import RestaurantsPage
+// import RestaurantsPage from './pages/RestaurantsPage'; // Import RestaurantsPage
 // thieu file
 const App: React.FC = () => {
   return (
@@ -19,7 +19,7 @@ const App: React.FC = () => {
           <Route path="/restaurants" element={<Restaurants />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/posts" element={<Posts />} />
-          <Route path="restaurants" element={<RestaurantsPage />} /> {/* Add route for RestaurantsPage */}
+          {/* <Route path="/restaurants" element={<RestaurantsPage />} />  */}
         </Routes>
         <Footer />
       </div>
